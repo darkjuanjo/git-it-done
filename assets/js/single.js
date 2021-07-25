d357019e5ccf2af56e3cd466da08123611f281c5
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-07-25 01:22:09 -0700

    added query parameters to html and linked repo search with issue display, also handled errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,10 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+//make a get request to url
 var getRepoIssues = repo => {
+    // request was successful
     var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
     fetch(apiUrl)
     .then(response => {
@@ -10,9 +14,15 @@ var getRepoIssues = repo => {
             .then(data => {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if(response.headers.get("Link")) {
+                   displayWarning(repo);
+                }
             });
         } else {
-            alert("There as a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -57,4 +67,33 @@ var displayIssues = issues => {
 
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = repo => {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+    linkEl.setAttribute("target", "_blank");
+
+    // append  to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+var getRepoName = () => {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
+getRepoName();
